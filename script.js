940a737e6a17b2dabc308cc52c7a8cc73b96bb68
Bhavin <bhavinpatel9624@gmail.com>
2024-03-16 00:34:03 +0530

    initial code added

---
+++ script.js
@@ -0,0 +1,7 @@
+const buttons = document.querySelectorAll(".btn");
+console.log(buttons);
+
+const greyBtn = document.getElementById("grey");
+const greenBtn = document.getElementById("green");
+const orangeBtn = document.getElementById("orange");
+const yellowBtn = document.getElementById("yellow");
